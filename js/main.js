db07d86e2f64b8e0c1b40f876dbfeca5dec18b96
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-11-21 14:38:51 -0800

    images and vibrant colors

--- js/main.js
+++ js/main.js
@@ -10,13 +10,44 @@ window.chartColors = {
 	grey: 'rgb(201, 203, 207)'
 };
 
+function rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+    }
+    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
+}
+
+function rainbowArray(n) {
+    var arr = [];
+    for (var i = 0; i <= n; i++) {
+        arr.push(rainbow(n, i));
+    }
+    return arr;
+}
+
 var skills = {
     "HTML5 / CSS3": 10,
-    "jQuery": 8,
+    "jQuery": 9,
     "Bootstrap": 10,
     "Angular 1 and 2": 7,
     "React": 3,
-    "Ionic (mobile apps)": 10
+    "Ionic (mobile apps)": 10,
+    "Django": 9,
+    "Wordpress": 10
 }
 
 var keys = [];
@@ -33,15 +64,7 @@ var config = {
     data: {
         datasets: [{
             data: values,
-            backgroundColor: [
-                window.chartColors.red,
-                window.chartColors.orange,
-                window.chartColors.yellow,
-                window.chartColors.green,
-                window.chartColors.blue,
-                window.chartColors.purple,
-                window.chartColors.grey
-            ],
+            backgroundColor: rainbowArray(keys.length),
             label: ''
         }],
         labels: keys
